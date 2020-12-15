08c364dc6dfd9f048de1a2223ff4c33fe66f7c58
Tamara <tcelist99@gmail.com>
2020-12-15 02:33:50 -0300

    Falta leer el archivo desde fuera

--- Juego.py
+++ Juego.py
@@ -11,8 +11,11 @@ import easy_shaders as es
 import lighting_shaders as ls
 import scene_graph2 as sg
 
+#Clase de la cracion de barras 
 class Barra:
     def __init__(self):
+        
+        #crea una barra
         gpu_barra = es.toGPUShape(bs.createColorCube(0.7, 0.4, 0))
             
         self.model = gpu_barra
@@ -22,6 +25,7 @@ class Barra:
         self.posy=0
         self.posz=0
         
+    #Dibuja una barra
     def Draw(self, color_pipeline, projection, view):
         if self.dibujo:
             self.transform = tr.matmul([tr.scale(2, 2, 0.3), tr.translate(self.posx, self.posy, self.posz)])
@@ -32,7 +36,7 @@ class Barra:
             glUniformMatrix4fv(glGetUniformLocation(color_pipeline.shaderProgram, 'model'), 1, GL_TRUE, self.transform)
             
             color_pipeline.drawShape(self.model)
-            
+    #Traslada las barras    
     def move1(self):
         self.posx=1.2
         self.posy=-1.7
@@ -55,20 +59,22 @@ class Barra:
         
     def moveUp(self,z):
         self.posz=z
-        
+ 
+#clase que crea las barras del archivo csv       
 class CreateBarras():
     def __init__(self):
         self.barras=[]
+        self.altura=0.5
     
     def create(self,estructura):
         with open(estructura) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-            altura = 0.3
+            #se lee el archivo
             for row in csv_reader:   
                 for i in range(9):
                     if row[0][i]=="1":
                         b=Barra()
-                        b.moveUp(altura*7)
+                        b.moveUp(self.altura*7)
                         if i==0:
                             b.move1()
                         elif i==2:
@@ -82,8 +88,8 @@ class CreateBarras():
                         self.barras.append(b)
                     if row[0][i]=="x":
                         b=Barra()
-                        b.moveUp(altura*7)
-                        b.real=False
+                        b.moveUp(self.altura*7)
+                        b.real=False #indica que una barra es falsa y si la toca el mono esta desaparece 
                         if i==0:
                             b.move1()
                         elif i==2:
@@ -95,16 +101,20 @@ class CreateBarras():
                         elif i==8:
                             b.move5()
                         self.barras.append(b)
-                altura += 1
+                #aumenta la altura para las proximas barras
+                self.altura += 1
+    #dibuja todas las barras
     def draw(self, pipeline, projection, view):
         for k in self.barras:
             k.Draw(pipeline, projection, view)
 
+#Crea los fondos 
 class Fondo():
     def __init__(self):
         self.models=[]
         self.transform=[]
         
+        #crea el "manto de un cubo 
         fondo1=bs.createTextureCube('fondo.jpg')
         gpufondo1 = es.toGPUShape(fondo1, GL_REPEAT, GL_NEAREST)
         self.models.append(gpufondo1)
@@ -124,7 +134,8 @@ class Fondo():
         gpufondo4 = es.toGPUShape(fondo4, GL_REPEAT, GL_NEAREST)
         self.models.append(gpufondo1)
         self.transform.append(tr.matmul([tr.uniformScale(24), tr.translate(1, 0, 0.4)]))
-        
+    
+    #dibuja todo los objetos creados en fondo
     def Draw(self, pipeline, projection, view):
         glUseProgram(pipeline.shaderProgram)
         for i in range (len(self.models)):
@@ -132,8 +143,32 @@ class Fondo():
             glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
             glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "view"), 1, GL_TRUE, view)
             pipeline.drawShape(self.models[i])
+
+#pone fondos azules a los costados simulando el cielo         
+class FondoAzul():
+    def __init__(self,count,fondo):
         
-    
+        fondoa1=bs.createTextureCube('azul.jpg')
+        gpufondoa1 = es.toGPUShape(fondoa1, GL_REPEAT, GL_NEAREST)
+        fondo.models.append(gpufondoa1)
+        fondo.transform.append(tr.matmul([tr.uniformScale(24), tr.translate(-1, 0, 0.4+count)]))
+        
+        fondoa2=bs.createTextureCube('azul.jpg')
+        gpufondoa2 = es.toGPUShape(fondoa2, GL_REPEAT, GL_NEAREST)
+        fondo.models.append(gpufondoa2)
+        fondo.transform.append(tr.matmul([tr.uniformScale(24), tr.translate(0, -1, 0.4+count)]))
+        
+        fondoa3=bs.createTextureCube('azul.jpg')
+        gpufondoa3 = es.toGPUShape(fondoa3, GL_REPEAT, GL_NEAREST)
+        fondo.models.append(gpufondoa3)
+        fondo.transform.append(tr.matmul([tr.uniformScale(24), tr.translate(0, 1, 0.4+count)]))
+        
+        fondoa4=bs.createTextureCube('azul.jpg')
+        gpufondoa4 = es.toGPUShape(fondoa4, GL_REPEAT, GL_NEAREST)
+        fondo.models.append(gpufondoa4)
+        fondo.transform.append(tr.matmul([tr.uniformScale(24), tr.translate(1, 0, 0.4+count)]))
+        
+
 # A class to store the application control
 class Controller:
     def __init__(self):
@@ -171,8 +206,29 @@ def readFaceVertex(faceDescription):
 
     return faceVertex
 
-
-
+#crea las flechas 
+class Flecha():
+    def __init__(self, posX, posY, posZ):
+        gpuarrow = es.toGPUShape(shape = readOBJ('Arrow.obj', (0,0,0)))
+        self.model=gpuarrow
+        self.posx=posX*0.2-4
+        self.posy=-posY*0.2
+        self.posz=posZ*0.2
+    #dibuja las flechas 
+    def draw(self, pipeline, projection, view):
+        glUseProgram(pipeline.shaderProgram)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "model"), 1, GL_TRUE,tr.matmul([  
+                tr.rotationZ(-np.pi/2),
+                tr.uniformScale(4),
+                tr.translate(self.posy, self.posx, self.posz)]))
+        pipeline.drawShape(self.model)
+    #mueve las flechas 
+    def update(self,dt):
+        self.posx+=dt
+             
+     
 def readOBJ(filename, color):
 
     vertices = []
@@ -249,6 +305,7 @@ if __name__ == "__main__":
     pipeline = ls.SimpleGouraudShaderProgram()
     texture_pipeline = es.SimpleTextureModelViewProjectionShaderProgram()
     color_pipeline = es.SimpleModelViewProjectionShaderProgram()
+    tex_pipeline=es.SimpleTextureTransformShaderProgram()
 
 
     # Telling OpenGL to use our shader program
@@ -260,10 +317,17 @@ if __name__ == "__main__":
     # As we work in 3D, we need to check which part is in front,
     # and which one is at the back
     glEnable(GL_DEPTH_TEST)
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-    # Creating shapes on GPU memory
-    gpuAmy = es.toGPUShape(shape = readOBJ('amy.obj', (0.9,0.6,0.2)))
+    # Creamos los objetos 
+    # Amy 
+    gpuAmy = es.toGPUShape(shape = readOBJ('amy.obj', (1,0,0.5)))
     
+    # Anillo (trofeo)
+    gpuAnillo = es.toGPUShape(shape = readOBJ('ring.obj', (0.9,0.9,0)))
+    
+    # Fondo
     fondo=Fondo()
         
     #Piso
@@ -271,33 +335,87 @@ if __name__ == "__main__":
     gpupiso = es.toGPUShape(piso, GL_REPEAT, GL_LINEAR)
     piso_transform = tr.matmul([tr.uniformScale(24), tr.translate(0, 0, -0.53)])
     
+    # Pantalla de inicio 
+    amyi = es.toGPUShape(bs.createTextureQuad("amy2.png", 1, 1), GL_REPEAT, GL_LINEAR)
+    
+    # Pantala de perdida 
+    eggman = es.toGPUShape(bs.createTextureQuad("eggman.png", 1, 1), GL_REPEAT, GL_LINEAR)
+    
+    # Pantalla de ganador 
+    amyf = es.toGPUShape(bs.createTextureQuad("am.png", 1, 1), GL_REPEAT, GL_LINEAR)
+    
+    # Barras
     barras=CreateBarras()
-    barras.create('estructura.csv')
+    barras.create('estructura1.csv')
     
-  #  b=Barra()
-   # b.moveUp(1)
-    
+    #Revisamos el tiempo
     t0 = glfw.get_time()
-
+    
+    # Posiciones de Amy
     posX=0
     posY=0
     posZ=0.5
-    grav=True
+    
+    #Cuenta los fondo que ya se crearon
+    count=1
+    
+    # Indica hasta que altura se debe llegar al saltar 
+    alt=0
+    
+    # Dice si se puede saltar o no 
+    salto=True
+    
+    #Indica cuando se "pierde" por saltar mal 
+    perder=False
+    
+    #Indica cuando se pierde por una flecha
+    perder2=False
+    
+    # Dice si se debe mandar una flecha de ataque 
+    arrow=True
+    
+    #Se crea la flecha con las posiciones hacia Amy
+    flechas=Flecha(posX, posY,posZ)
+    
+    #Tamaño inicial de la imagen de partida, de perdida y de ganador
+    ag=2
+    eg=0
+    af=0
+    
+    # rotaciones de Amy
+    rotz=np.pi
+    rotx=np.pi/2
+    roty=0
+    
+    #Indica si ya estuvo en una barra 
+    bt=False
+    
+    #Rotaciones y posiciones del aro 
+    ra=0
+    x=0
+    y=0
+    #Indica si gana o pierde 
+    ganar=False
+
     while not glfw.window_should_close(window):
         # Using GLFW to check for input events
         glfw.poll_events()
+
 
         # Getting the time difference from the previous iteration
         t1 = glfw.get_time()
         dt = t1 - t0
         t0 = t1
-    
+        
+        #Se define la vista "fija" del juego 
         view = tr.lookAt(
-            np.array([12, -3,8+(posZ-0.5)]),
+            np.array([12,-5,8+(posZ-0.5)]),
             np.array([posX*0.4,posY*0.4,posZ]),
             np.array([0,0,1])
         )
-
+        
+        #Cambia la vista mientras se mantengan apretadas las teclas 
+        
         if (glfw.get_key(window, glfw.KEY_B) == glfw.PRESS):
              view = tr.lookAt(
                  np.array([0,1,17.5+(posZ)]),
@@ -305,7 +423,7 @@ if __name__ == "__main__":
                  np.array([0,0,1]))
         if (glfw.get_key(window, glfw.KEY_N) == glfw.PRESS):
              view = tr.lookAt(
-                 np.array([9,9,7+posZ]),
+                 np.array([-12,-5,7.5+posZ]),
                  np.array([posX,posY,posZ]),
                  np.array([0,0,1]))
         if (glfw.get_key(window, glfw.KEY_M) == glfw.PRESS):
@@ -320,48 +438,98 @@ if __name__ == "__main__":
         # Clearing the screen in both, color and depth
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
+        #Dibujamos el fondo 
         fondo.Draw(texture_pipeline, projection, view)
+        
+        # Revisamos si se debe crear otro fondo
+        if posZ>13*count:
+            fondoazul=FondoAzul(count, fondo)
+            count+=1
+        
+        #Dibujamos el piso           
         glUseProgram(texture_pipeline.shaderProgram)
         glUniformMatrix4fv(glGetUniformLocation(texture_pipeline.shaderProgram, "model"), 1, GL_TRUE, piso_transform)
         glUniformMatrix4fv(glGetUniformLocation(texture_pipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
         glUniformMatrix4fv(glGetUniformLocation(texture_pipeline.shaderProgram, "view"), 1, GL_TRUE, view)
         texture_pipeline.drawShape(gpupiso)
         
-               
-        
-        # Drawing shapes
+        # Luz 
         glUseProgram(pipeline.shaderProgram)
         glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "La"), 1.0, 1.0, 1.0)
         glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ld"), 1.0, 1.0, 1.0)
         glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ls"), 1.0, 1.0, 1.0)
 
-        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ka"), 0.2, 0.2, 0.2)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ka"), 0.3, 0.3, 0.3)
         glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Kd"), 0.9, 0.9, 0.9)
-        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ks"), 0.5, 0.5, 0.5)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ks"), 0.2, 0.2, 0.2)
 
-        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "lightPosition"), 0, 3, 3)
-        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "viewPosition"), 0, 0, 7)
-        glUniform1ui(glGetUniformLocation(pipeline.shaderProgram, "shininess"), 10)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "lightPosition"), 0, 0, posZ+5)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "viewPosition"), 0, 0, posZ+5)
+        glUniform1ui(glGetUniformLocation(pipeline.shaderProgram, "shininess"), 1000)
         glUniform1f(glGetUniformLocation(pipeline.shaderProgram, "constantAttenuation"), 0.01)
         glUniform1f(glGetUniformLocation(pipeline.shaderProgram, "linearAttenuation"), 0.1)
         glUniform1f(glGetUniformLocation(pipeline.shaderProgram, "quadraticAttenuation"), 0.01)
         
+        #Dibujamos a Amy
         glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
         glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "view"), 1, GL_TRUE, view)
         glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "model"), 1, GL_TRUE,tr.matmul([         
                 tr.translate(posX, posY, posZ),
-                tr.rotationZ(np.pi),
-                tr.rotationX(np.pi/2),
+                tr.rotationZ(rotz),
+                tr.rotationX(rotx),
+                tr.rotationY(roty),
                 tr.uniformScale(0.4)]))
         pipeline.drawShape(gpuAmy)
+        
+        #Dibujamos el anillo a la altura de la ultima barra
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "model"), 1, GL_TRUE,tr.matmul([         
+                tr.translate(x, y, barras.altura*2),
+                tr.rotationZ(ra),
+                tr.uniformScale(0.5)]))
+        pipeline.drawShape(gpuAnillo)
+        
+        # Dibujamos la pantalla inicial
+        glUseProgram(tex_pipeline.shaderProgram)
+        glUniformMatrix4fv(glGetUniformLocation(tex_pipeline.shaderProgram, "transform"), 1, GL_TRUE, tr.matmul([
+            tr.translate(0, 0, 0),
+            tr.uniformScale(ag)]))
+        tex_pipeline.drawShape(amyi)
+        
+        # Dibujamos la pantalla de perdida 
+        glUniformMatrix4fv(glGetUniformLocation(tex_pipeline.shaderProgram, "transform"), 1, GL_TRUE, tr.matmul([
+            tr.translate(0, 0, 0),
+            tr.uniformScale(eg)]))
+        tex_pipeline.drawShape(eggman)
+        
+        # Dibujamos la pantalla de ganador  
+        glUniformMatrix4fv(glGetUniformLocation(tex_pipeline.shaderProgram, "transform"), 1, GL_TRUE, tr.matmul([
+            tr.translate(0, 0, 0),
+            tr.uniformScale(af)]))
+        tex_pipeline.drawShape(amyf)
+        
+        # Hace una copia de la lista de barras 
         b=barras.barras
-      #  if posZ>0.6 and grav==True:
-       #     posZ-=3*dt
+        
+        #Indica que hay gravedad por lo cual amy va a caer mientras no este en una plataforma 
+        grav=True
+        
+        #Cambia el angulo de de rotación del aro 
+        ra-=2*dt
+        
+        #Revisa que la posición de las barras calza con Amy 
         for i in range(len(b)):
-            if posX<=2*b[i].posx+1 and posX>=2*b[i].posx-1 and posY<=2*b[i].posy+1 and posY>=2*b[i].posy-1 and posZ<=b[i].posz*0.3+1.5 and posZ>=b[i].posz*0.3+1.25:
-                grav=False
-            else:
-                grav=True
+            if posX<=2*b[i].posx+1 and posX>=2*b[i].posx-1 and posY<=2*b[i].posy+1 and posY>=2*b[i].posy-1 and posZ<=b[i].posz*0.3+1.4 and posZ>=b[i].posz*0.3+1.25:
+                if b[i].real==False: #Revisa si la barra es real, de serlo no la dibuja y no apaga la gravedad 
+                    b[i].dibujo=False
+                else:
+                    grav=False #si no, apaga la gravedad 
+                    bt=True #indica que ya se subio una barra 
+                    
+        # Implementa la gravedad 
+        if posZ>0.6 and grav==True:
+            posZ-=3*dt
+            
+        #Mueve a Amy dependiendo de la tecla que se precione 
         if (glfw.get_key(window, glfw.KEY_A) == glfw.PRESS):
             if posX>-6:
                 posX-=5*dt
@@ -374,14 +542,66 @@ if __name__ == "__main__":
         if (glfw.get_key(window, glfw.KEY_S) == glfw.PRESS):
             if posY>-6:
                 posY-=5*dt
+        #Implementa los saltos 
         if (glfw.get_key(window, glfw.KEY_SPACE) == glfw.PRESS):
-            posZ+=6*dt
-            
-        print ("amy x"+"   z"+str(posZ))
-        print (str(b[0].posz))
-       # print ("bra x"+str(b.posx)+"   y"+str(b.posy)+"   z"+str(b.posz))
-       # b.Draw(color_pipeline, projection, view)
+            #si se puede saltar
+            if salto==True: 
+                salto=False #se bloquea otro salto
+                alt=2+posZ #calcula hasta donde debe llegar 
+            #si no pierde y no llega al limite de altura
+            if alt>=posZ and perder==False: 
+                posZ+=5*dt #salta 
+            #Si esta en una barra y aún no pierde 
+            if grav==False and perder2==False:
+                salto=True #Puede volver a saltar 
+                perder=False #aún no pierde 
+            #Inidica que perdio 
+            if alt<posZ+0.01:
+                perder=True
+        # Si es que se puede hacer una flecha 
+        if arrow==True:
+            ti=t1 #Revisamos el tiempo actual 
+            arrow=False #decimos que no se pueden hacer mas flechas
+            flechas=Flecha(posX,posY,posZ) #se indica las posicones de la flecha 
+        # Si ya pasa un tiempo especifico
+        if ti+3<t1:
+            arrow=True # se puede hacer otra flecha 
+        #Revisamos que una flecha toca a Amy
+        if flechas.posx<=posX+0.1 and flechas.posx>=posX-0.1 and flechas.posy<=-posY*0.2+0.1 and flechas.posy>=-posY*0.2-0.1 and flechas.posz<=posZ*0.2+0.1 and flechas.posz>=posZ*0.2-0.1:
+            perder2=True #Pierde
+        #Mueve las flechas a partir de un tiempo en especifico 
+        if t1>8:
+            flechas.update(4*dt)
+            flechas.draw(pipeline, projection, view) 
+        #Hace el movimiento de la imagen inicial 
+        if t1<8: 
+            if ag>0:
+                ag-=dt    
+        #Si pierde genera la imagen de game over y Amy empieza a rotar 
+        if ((posZ>=0.5 and posZ<=0.6 and grav and bt) or perder2 or (salto==False and posZ>=0.5 and posZ<=0.6) )and ganar==False:
+            if eg<2:
+                eg+=0.8*dt
+            rotx-=2*dt
+            rotz-=2*dt
+        #Indica cuando gana 
+        if barras.altura*2-posZ<=0.5 and barras.altura*2-posZ>=0 and grav==False:
+            ganar=True
+            #Mueve el aro a Amy 
+            if x<1.7*posX:
+                x+=5*dt
+            elif x>=1.7*posX:
+                x-=5*dt   
+            if y<1*posY:
+                y+=5*dt
+            elif y>=1*posY:
+                y-=5*dt
+            #Hace el movimiento de la imagen de ganador 
+            if af<2:
+                af+=0.8*dt
+                
+        #Dibuja las barras 
         barras.draw(color_pipeline,projection, view)
+        
         # Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
         glfw.swap_buffers(window)
         
--- Sin título0.py
+++ Sin título0.py
@@ -1,13 +1,35 @@
-import csv 
-def create(estructura):
-        with open(estructura) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            line_count = 0
-            for row in csv_reader:
-                for i in range(9):
-                    print(row[0][i])
-                print(row)
-               
-                line_count+=1
-            print(line_count)
-create('estructura.csv')
+# Por: Benja Vera
+# Nota: Ya que esto es para una ocasión super puntual y todo el mundo involucrado probablemente sabe programar mejor que yo,
+# no me voy a molestar en chequear inputs :)
+
+horasTotales = 240
+diaPrimeraEntrega = int(input("¿Qué día de octubre entregaste la tarea 1? (ej: 24)\n"))
+horaPrimeraEntrega = int(input("Genial ¿Y a qué hora? (ingrese un solo entero de 0 a 23, ej: 15)\n"))
+if diaPrimeraEntrega <= 14:
+    horas1 = 0
+elif 15 <= diaPrimeraEntrega <= 17:  # primeros días
+    horas1 = (diaPrimeraEntrega - 15)*24 + horaPrimeraEntrega
+elif 18 <= diaPrimeraEntrega <= 25:  # receso, las 72 horas se congelan
+    horas1 = 72
+else:
+    horas1 = 72 + (diaPrimeraEntrega - 26)*24 + horaPrimeraEntrega  # luego, desde el lunes 26, cuentan a partir de 72
+
+if horas1 > 240:  # si con la T1 sobrepasa el límite...
+    raise Exception('F')  # u.u
+print("Bacán, usaste " + str(horas1) + " horas del total de 240 en la primera tarea.")
+
+# Nota a mi yo del futuro: Cambiar de aquí en adelante para la tarea 3
+
+horasR = horasTotales - horas1
+diasRestantes = horasR//24  # división entera
+horasRestantes = horasR%24  # módulo
+print("Hagamos un resumen: te quedan " + str(horasR) + " horas en total luego de las que usaste para la T1. Esto se traduce a "\
+     + str(diasRestantes) + " días y " + str(horasRestantes) + " horas (no estoy considerando el atraso que ya puedes llevar en la T2)")
+
+if diasRestantes <= 2:  # si no se pasa del domingo, da lo mismo
+    print("Esto significa que puedes entregar hasta el día " + str(10 + diasRestantes) + " a las " + str(horasRestantes) + " hrs sin descuento :)")
+else: # aquí, tenemos que no considerar el domingo
+    print("Teniendo en mente que los domingos no descuentan, esto significa que puedes entregar hasta el día " + str(10 + diasRestantes + 1) \
+        + " a las " + str(horasRestantes) + " hrs sin descuento :)")
+
+out = input("Mucho ánimo con lo que queda del semestre! Presiona ENTER para salir: ")
